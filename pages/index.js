707253966ff07b95308747da0dcb96ce6b85d27c
Stephen Smith <stephenlehanesmith@gmail.com>
2023-02-17 14:51:06 +0000

    added linkedin and github svg logos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import utilStyles from '../styles/utils.module.css';
+import homeStyles from "../styles/Home.module.css";
 import Layout, { siteTitle } from '../components/layout';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,9 +14,29 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <div className={homeStyles.homeLayout}>
       <section className={utilStyles.headingMd}>
         <p><Link href="https://github.com/inherentspice/VKTest-Django-Frontend">From automated chatbots trained with scraped reddit comments</Link>, to <Link href="https://hig.fly.dev/">depicting our collective mental state on cans of beer </Link>— I flat out love building cool stuff for the web.</p>
       </section>
+      <div className={homeStyles.socialCont}>
+        <Link href="https://github.com/inherentspice" target="_blank">
+          <Image
+            src="/images/github-mark.svg"
+            height={30}
+            width={30}
+            alt=""
+          />
+        </Link>
+        <Link href="https://www.linkedin.com/in/stephen-lehane-smith/" target="_blank">
+          <Image
+            src="/images/linkedin.svg"
+            height={30}
+            width={30}
+            alt=""
+          />
+        </Link>
+      </div>
+    </div>
     </Layout>
   )
 }
