3a83f7e30f056ebcb59154f33b87f086dab1fe38
Stephen Smith <stephenlehanesmith@gmail.com>
2023-02-17 14:14:45 +0000

    initialized basic home page layout, added hamburger menu

---
+++ components/hamburgerMenu.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import hamburgerStyles from "./hamburgerMenu.module.css"
+import Link from 'next/link';
+
+export default function HamburgerMenu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function handleMenuClick() {
+    setMenuOpen(!menuOpen);
+  }
+
+  return (
+    <>
+      <button className={`${hamburgerStyles.menuButton} ${menuOpen ? hamburgerStyles.menuToggle : ''}`} onClick={handleMenuClick}>
+        <span className={hamburgerStyles.menuIcon}></span>
+      </button>
+      <nav className={`${hamburgerStyles.menu} ${menuOpen ? hamburgerStyles.menuOpen : ''}`}>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/portfolio">Portfolio</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </>
+  );
+}
