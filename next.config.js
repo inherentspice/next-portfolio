a450799fcb2ca70543343f0c2f646ab9dc934b94
Stephen Smith <stephenlehanesmith@gmail.com>
2023-02-18 19:29:16 +0000

    completed first draft of contact page with emailjs form

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    REACT_APP_EMAILJS_SERVICE_ID: process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    REACT_APP_EMAILJS_TEMPLATE_ID: process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    REACT_APP_EMAILJS_USER_ID: process.env.REACT_APP_EMAILJS_USER_ID
+  }
 }
 
 module.exports = nextConfig
