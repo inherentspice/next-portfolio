bfe388a6c8d8034d23a6724a00f94c507e14cce1
inherentspice <stephenlehanesmith@gmail.com>
2023-03-04 19:45:50 +0000

    renamed public variables

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -21,10 +21,10 @@ export default function Contact() {
   const sendEmail = (data, e) => {
     e.preventDefault()
     emailjs.send(
-      process.env.REACT_APP_EMAILJS_SERVICE_ID,
-      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
       data,
-      process.env.REACT_APP_EMAILJS_USER_ID,
+      process.env.NEXT_PUBLIC_EMAILJS_USER_ID,
 
     )
       .then(resetForm())
